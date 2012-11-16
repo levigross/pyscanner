d530feb9dc2e1441e5b184bbe991c37a9aeebe5d
Levi Gross <levi@levigross.com>
2012-11-15 23:18:57 -0500

    Broke Pyflakes and added my own functionality to list functions and imports more to come

---
+++ mytests/main.py
@@ -0,0 +1,21 @@
+import urllib
+
+def func1():
+    pass
+
+
+def func2():
+    pass
+
+
+def func3():
+    pass
+
+
+def main():
+    func1()
+    func2()
+    func3()
+
+if __name__ == '__main__':
+    main()
